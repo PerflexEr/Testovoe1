67a47cdc82e43e8192b9b559c966a54a4c229aea
PerflexEr <stepasorokin56@gmail.com>
2024-01-19 17:57:01 +0200

    desktop version rdy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
 import Menu from "./components/menu/menu";
-import PagesNavBar from "./components/pagesNavbar/pagesNavBar";
-import Customer from "./components/customer/customer";
 import Header from "./components/header/header";
-import Search from "./components/search/search";
 import Customers from "./components/customers/customers";
+
 function App() {
   return (
-   <div className="container">
-      <Customers/>
+    <div className="app">
+      <Menu/>
+      <div className="container">
+        <Header className='header'/>
+        <Customers/>
+      </div>
     </div>
   );
 }
