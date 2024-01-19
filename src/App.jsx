995461332321713fa047d3710768ecd25af0fd74
PerflexEr <stepasorokin56@gmail.com>
2024-01-19 16:09:27 +0200

    customer overview rdy"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import Menu from "./components/menu/menu";
 import PagesNavBar from "./components/pagesNavbar/pagesNavBar";
+import Customer from "./components/customer/customer";
 function App() {
   return (
    <div className="">
-      <PagesNavBar/>
+      <Customer name={'Jane Cooper'} company={'Microsoft'} number={'(225) 555-0118'} email={'jane@microsoft.com'} country={'United States'} status={'active'} />
+      <Customer  />
+      <Customer name={'Jane Cooper'} company={'Microsoft'} number={'(225) 555-0118'} email={'jane@microsoft.com'} country={'United States'} status={'inactive'} />
     </div>
   );
 }
---
+++ src/components/customer/customer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './customer.scss';
+
+const Customer = ({ name = 'Customer Name', company = 'Company', number = 'Phone Number', email = 'Email', country = 'Country', status = 'Status' }) => {
+  return (
+    <div>
+      {status === 'Status' ? 
+        <div className="customer" style={{color: '#B5B7C0'}}>
+          <div className="customer__name">{name}</div>
+          <div className="customer__company">{company}</div>
+          <div className="customer__number">{number}</div>
+          <div className="customer__email">{email}</div>
+          <div className="customer__country">{country}</div>
+          <div className="customer__status" style={{width:'80px' , textAlign:'center'}}>{status}</div>
+        </div> : (
+        <div className="customer">
+          <div className="customer__name">{name}</div>
+          <div className="customer__company">{company}</div>
+          <div className="customer__number">{number}</div>
+          <div className="customer__email">{email}</div>
+          <div className="customer__country">{country}</div>
+          <div className="customer__status">
+            {status === 'active' ?
+              <div className="active__status">Active</div> :
+              <div className="inactive__status">Inactive</div>
+            }
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Customer;
